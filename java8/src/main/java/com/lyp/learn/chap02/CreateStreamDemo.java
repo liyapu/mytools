f94715a71c42b24006744960fbe60ebb903e3489
liyaupu <liyapu@tusdao.com>
2019-09-26 20:49:51 +0800

    stream 生产

--- java8/src/main/java/com/lyp/learn/chap02/CreateStreamDemo.java
+++ java8/src/main/java/com/lyp/learn/chap02/CreateStreamDemo.java
@@ -1,54 +1,89 @@
 package com.lyp.learn.chap02;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/**
+ * 获取Stream
+ */
 public class CreateStreamDemo {
-    public static void main(String[] args) {
-        //由值创建流
-        System.out.println("----------由值创建流---------------");
+
+    /**
+     * 由值创建流
+     */
+    @Test
+    public void testNum(){
         Stream<String> strStream = Stream.of("aa","bb","ff","bb","gg");
         strStream.forEach(s -> System.out.print(s +", "));
-        System.out.println("\r\n");
+    }
 
-        //创建一个空流
+    /**
+     * 创建一个空流
+     */
+    public void testEmptyStream(){
         Stream<String> strStream2 = Stream.empty();
+    }
 
-        //由数组创建流
-        System.out.println("--------由数组创建流---------");
+    /**
+     * 由数组创建流
+     */
+    @Test
+    public void testArray(){
         int [] intArr = {5,3,2,7,8,3};
         IntStream intStream = Arrays.stream(intArr);
         int intSum = intStream.sum();
         System.out.println(intSum);
-        System.out.println();
+    }
 
-        //由文件生成流
-        System.out.println("--------由文件生成流------------");
-//        long uniqueWords = 1;
-//        try(
-//                //todo Paths 路径，读取文件，待学习解决
-//           Stream<String> lines = Files.lines(Paths.get("data.txt"), Charset.forName("utf-8"))
-//        ) {
-//            uniqueWords = lines.flatMap(line -> Arrays.stream(line.split(" ")))
-//                    .distinct()
-//                    .count();
-//        }
-//        catch(IOException e){
-//            e.printStackTrace();
-//        }
-//        System.out.println(uniqueWords);
-        System.out.println();
-
-
-        //由函数生成流：创建无限流
-        System.out.println("-----------由函数生成流：创建无限流---------------");
+    /**
+     * 由函数生成流：创建无限流
+     */
+    @Test
+    public void testFunctionStream(){
         List<Integer> intList =  Stream.iterate(5, n -> n + 5)
                                         .limit(10)
                                         .collect(Collectors.toList());
         System.out.println(intList);
+
+        System.out.println("-----------");
+
+        Stream.generate(Math::random)
+                .limit(5)
+                .forEach(t -> System.out.print(t + " , "));
+    }
+
+    /**
+     * 由文件生成流
+     */
+    @Test
+    public void  testFileStream(){
+        List<String> uniqueWords = new ArrayList<>();
+        try(Stream<String> lines = Files.lines(Paths.get(Thread.currentThread().getContextClassLoader().getResource("").getPath(),"data.txt"), Charset.forName("utf-8"))) {
+            uniqueWords = lines.flatMap(line -> Arrays.stream(line.split(" ")))
+                                .distinct()
+                                .collect(Collectors.toList());
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        uniqueWords.forEach(System.out::println);
+    }
+
+    /**
+     * 斐波纳契
+     */
+    @Test
+    public void testFeiBoNaQie(){
         //斐波纳契数列
         Stream.iterate(new int [] {0,1},t -> new int [] {t[1],t[0] + t[1]})
                 .limit(10)
@@ -60,13 +95,6 @@ public class CreateStreamDemo {
         Stream.iterate(new int [] {0,1}, t -> new int [] {t[1],t[0] + t[1]})
                 .limit(10)
                 .forEach(t -> System.out.print("(" + t[0] + "," + t[1] + ")  " ));
-
-        System.out.println();
-
-        Stream.generate(Math::random)
-                .limit(5)
-                .forEach(t -> System.out.print(t + " , "));
-        System.out.println();
-
     }
+
 }
