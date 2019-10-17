b346e28d67df3fe6afbe1004548c41ea24c3e1a2
liyaupu <liyapu@tusdao.com>
2019-10-17 15:59:25 +0800

    beanutils

---
+++ tools/src/main/java/com/lyp/learn/jdk/IntrospectorTest.java
@@ -0,0 +1,48 @@
+package com.lyp.learn.jdk;
+
+import com.lyp.learn.bean.User;
+import org.junit.jupiter.api.Test;
+
+import java.beans.*;
+import java.lang.reflect.Method;
+
+/**
+ * @author: liyapu
+ * @description:
+ * @date 2019-10-17 15:27
+ *
+ * Introspector(内省)是jdk提供的用于描述Java bean支持的属性、方法以及事件的工具；
+ * 利用此类可得到BeanInfo接口的实现对象:
+ *
+ */
+public class IntrospectorTest {
+
+    /**
+     * getBeanDescriptor()返回的BeanDescriptor提供了java bean的一些全局的信息，如class类型、类名称等。
+     *
+     * getPropertyDescriptors()返回PropertyDescriptor[]，PropertyDescriptor描述了java bean中一个属性和它们的getter & setter方法的SoftReference。
+     */
+    @Test
+    public void test01() throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(User.class);
+        BeanDescriptor beanDescriptor = beanInfo.getBeanDescriptor();
+
+        System.out.println(beanDescriptor.getBeanClass());
+        System.out.println(beanDescriptor.getName());
+        System.out.println(beanDescriptor.getDisplayName());
+        System.out.println(beanDescriptor.getShortDescription());
+        System.out.println("--------------------");
+
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+            Method readMethod = propertyDescriptor.getReadMethod();
+            Method writeMethod = propertyDescriptor.getWriteMethod();
+
+            System.out.println(propertyDescriptor.getName());
+            System.out.println(propertyDescriptor.getDisplayName());
+            System.out.println(propertyDescriptor.getShortDescription());
+            System.out.println(readMethod);
+            System.out.println(writeMethod);
+        }
+    }
+}
--- tools/src/main/java/com/lyp/learn/utils/MyBeanUtils.java
+++ tools/src/main/java/com/lyp/learn/utils/MyBeanUtils.java
@@ -9,6 +9,8 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
@@ -18,10 +20,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -148,6 +147,34 @@ public class MyBeanUtils {
         BeanUtils.copyProperties(source,t);
         return t;
     }
+
+    /**
+     * 注意1：Spring 的BeanUtils.copyProperties在拷贝属性时忽略空值解决办法。
+     * 当src对象的键值为Null时，就会把target对象的对应键值覆盖成空了。
+     */
+    public static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<>();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null){
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    /**
+     * 复制，忽略source中为null的字段，不使其覆盖target中有值而且与source中为null的字段对应的
+     * @param source
+     * @param target
+     */
+    public static void copyPropertiesIgnoreNull(Object source, Object target){
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
 
     /**
      * apache commons org.apache.commons.BeanUtils
