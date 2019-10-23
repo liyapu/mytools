c5940914ac4e756a247b7f0372d5e0d88652cb66
liyaupu <liyapu@tusdao.com>
2019-10-23 10:04:10 +0800

    反射

--- open-tools/src/main/java/com/lyp/learn/apachecommons/bean/ReflectBean.java
+++ open-tools/src/main/java/com/lyp/learn/apachecommons/bean/ReflectBean.java
@@ -18,4 +18,39 @@ public class ReflectBean {
         this.address = address;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getInfo(String msg){
+        return "info msg is:" + msg;
+    }
+    public String getInfo(String msg,Integer level) {
+        return "info msg is :" + msg + ", level is " + level;
+    }
+
+    public static String staticHello(){
+        return "static method hello";
+    }
 }
--- open-tools/src/main/java/com/lyp/learn/apachecommons/lang3/reflect/MethodUtilsTest.java
+++ open-tools/src/main/java/com/lyp/learn/apachecommons/lang3/reflect/MethodUtilsTest.java
@@ -1,7 +1,10 @@
 package com.lyp.learn.apachecommons.lang3.reflect;
 
 import com.lyp.learn.apachecommons.bean.ReflectBean;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author: liyapu
@@ -11,7 +14,23 @@ import org.junit.jupiter.api.Test;
 public class MethodUtilsTest {
 
     @Test
-    public void test01(){
+    public void test01() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         ReflectBean rb = new ReflectBean();
+
+        // 调用无参方法
+        MethodUtils.invokeMethod(rb, "getAge", null);
+
+        // 调用一参方法
+        Object getInfo1 = MethodUtils.invokeMethod(rb, "getInfo", "Hello");
+        System.out.println("getInfo1 : " + getInfo1);
+
+        // 调用多参方法
+        Object getInfo2 = MethodUtils.invokeMethod(rb, "getInfo", new Object[]{"特大消息", new Integer(10)});
+        System.out.println("getInfo2 : " + getInfo2);
+
+        // 调用静态方法
+        Object staticHello = MethodUtils.invokeStaticMethod(ReflectBean.class, "staticHello", null);
+        System.out.println("staticHello : " + staticHello);
+
     }
 }
