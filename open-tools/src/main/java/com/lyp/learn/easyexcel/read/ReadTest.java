00fe419edd7f8cfef75e224a779ec466a3f4f5ce
liyaupu <liyapu@tusdao.com>
2020-01-20 18:38:41 +0800

    exc

--- open-tools/src/main/java/com/lyp/learn/easyexcel/read/ReadTest.java
+++ open-tools/src/main/java/com/lyp/learn/easyexcel/read/ReadTest.java
@@ -1,6 +1,8 @@
 package com.lyp.learn.easyexcel.read;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.read.metadata.ReadSheet;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -27,7 +29,13 @@ public class ReadTest {
         String fileName = getFileName();
 
         //这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
-        EasyExcel.read(fileName, Hospital.class, new HospitalListener()).sheet().doRead();
+//        EasyExcel.read(fileName, Hospital.class, new HospitalListener()).sheet().doRead();
+
+        //指定 sheet
+//        EasyExcel.read(fileName, Hospital.class, new HospitalListener()).sheet(0).doRead();
+
+        //指定 sheet ,指定标题行
+        EasyExcel.read(fileName, Hospital.class, new HospitalListener()).sheet(0).doRead();
     }
 
     private String getFileName() {
@@ -38,14 +46,12 @@ public class ReadTest {
 
     @Test
     public void simpleRead2() {
-//        // 有个很重要的点 DemoDataListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
-//
-//        // 写法2：
-//        fileName = TestFileUtil.getPath() + "demo" + File.separator + "demo.xlsx";
-//        ExcelReader excelReader = EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).build();
-//        ReadSheet readSheet = EasyExcel.readSheet(0).build();
-//        excelReader.read(readSheet);
-//        // 这里千万别忘记关闭，读的时候会创建临时文件，到时磁盘会崩的
-//        excelReader.finish();
+        String fileName = getFileName();
+
+        ExcelReader excelReader = EasyExcel.read(fileName, Hospital.class, new HospitalListener()).build();
+        ReadSheet readSheet = EasyExcel.readSheet(0).build();
+        excelReader.read(readSheet);
+        // 这里千万别忘记关闭，读的时候会创建临时文件，到时磁盘会崩的
+        excelReader.finish();
     }
 }
