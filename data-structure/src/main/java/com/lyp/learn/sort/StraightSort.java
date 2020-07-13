a3adfef74b0a89ac48c8bd3c6ee9e0df8ec40154
liyapugitglobal <liyapu20116197@163.com>
2020-07-13 23:42:15 +0800

    ds

--- data-structure/src/main/java/com/lyp/learn/sort/StraightSort.java
+++ data-structure/src/main/java/com/lyp/learn/sort/StraightSort.java
@@ -1,11 +1,58 @@
 package com.lyp.learn.sort;
 
+import java.util.Arrays;
+
 public class StraightSort {
 
-    public static void printArray(int [] array){
+    public static void main(String[] args) {
+        int[] array = new int[]{52, 39, 67, 95, 70, 8, 25, 52};
+        System.out.println("排序前: " + Arrays.toString(array));
+        array = straightSort(array);
+        System.out.println("排序后: " + Arrays.toString(array));
+        System.out.println();
+
+        int[] arr = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
+        System.out.println("排序前: " + Arrays.toString(arr));
+        straightSort2(arr);
+        System.out.println("排序后: " + Arrays.toString(arr));
+        System.out.println();
+
+
+    }
+
+    //------------------------- 方式一 ----------------------------------------------
+
+    /**
+     * 直接插入排序
+     *
+     * @param array
+     * @return
+     */
+    public static int[] straightSort(int[] array) {
         int length = array.length;
-        for(int i = 0; i < length; i++){
-            if(i != 0){
+        if (length == 0) {
+            return array;
+        }
+        //第一层控制：顺序往后：从第一个元素到末尾
+        for (int i = 1; i < length; i++) {
+            //第二层控制：逆序往前逐个比较:
+            // j > 0 ,j的最小值是 1,所以下面的 j-1 不会数组越界
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+
+    public static void printArray(int[] array) {
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            if (i != 0) {
                 System.out.print(" , ");
             }
             System.out.print(array[i]);
@@ -13,35 +60,22 @@ public class StraightSort {
         System.out.println();
     }
 
-    /**
-     * 直接插入排序
-     * @param array
-     * @return
-     */
-    public static int [] straightSort(int [] array){
-        int length = array.length;
-        if(length == 0){
-            return array;
+    //------------------------- 方式二 ----------------------------------------------
+    public static void straightSort2(int[] arr) {
+        int len;
+        if (arr == null || (len = arr.length) == 0) {
+            return;
         }
-        //第一层控制：顺序往后：从第一个元素到末尾
-        for(int i = 1; i < length; i++){
-            //第二层控制：逆序往前逐个比较:
-            for(int j = i; j > 0; j--){
-                if(array[j] < array[j-1]){
-                    int temp = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = temp;
-                }
+        for (int i = 1; i < len; i++) {
+            int j = i;
+            //此处 j >= 1, 所以 j-1 不会数组越界
+            while (j >= 1 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
             }
         }
-        return array;
-    }
-
-    public static void main(String[] args) {
-        int [] array = new int [] {52,39,67,95,70,8,25,52};
-        printArray(array);
-        array = straightSort(array);
-        printArray(array);
     }
 
 
--- data-structure/src/test/java/com/lyp/learn/sort/BubbleSortTest.java
+++ data-structure/src/test/java/com/lyp/learn/sort/BubbleSortTest.java
@@ -1,59 +1,37 @@
-//package com.lyp.learn.sort;
-//
-//
-//import java.util.Arrays;
-//
-///**
-// * @author: liyapu
-// * @description:
-// * @date 2020-07-13 15:48
-// */
-//class BubbleSortTest {
-//    public static void main(String[] args) {
-//        int[] arr = new int[]{3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
-////        int[] arr = new int[]{5,4,3,2,1,6,9,7,8};
-//         int [] a = mergeSort(arr);
-//        System.out.println(Arrays.toString(a));
-//    }
-////
-////    public static int[] mergeSort(int[] arr){
-////        int len;
-////        if(arr == null || (len = arr.length) == 0){
-////            return arr;
-////        }
-////        if(len < 2){
-////            return arr;
-////        }
-////        int middle = len/ 2;
-////        int [] leftArr = Arrays.copyOfRange(arr,0,middle);
-////        int[] rightArr = Arrays.copyOfRange(arr,middle,len);
-////        return merge(mergeSort(leftArr),mergeSort(rightArr));
-////    }
-////
-////    private static int[] merge(int[] leftArr, int[] rightArr) {
-////        int leftLen = leftArr.length;
-////        int rightLen = rightArr.length;
-////        int len = leftLen + rightLen;
-////        int [] arr = new int[len];
-////        int i = 0,j = 0;
-////        int index = 0;
-////        while(i < leftLen && j < rightLen){
-////            if(leftArr[i] < rightArr[j]){
-////                arr[index++] = leftArr[i++];
-////            }else if(leftArr[i] >=rightArr[j]){
-////                arr[index++] = rightArr[j++];
-////            }
-////        }
-////        while(i < leftLen){
-////            arr[index++] = leftArr[i++];
-////        }
-////        while(j < rightLen){
-////            arr[index++] = rightArr[j++];
-////        }
-////        return arr;
-////    }
-//
-//
-//
-//
-//}
+package com.lyp.learn.sort;
+
+
+import java.util.Arrays;
+
+/**
+ * @author: liyapu
+ * @description:
+ * @date 2020-07-13 15:48
+ */
+class BubbleSortTest {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
+        System.out.println(Arrays.toString(arr));
+         straightSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public  static void straightSort(int [] arr){
+        int len;
+        if(arr == null || (len = arr.length) == 0){
+            return;
+        }
+        for (int i = 1; i < len ; i++) {
+            int j = i;
+            while(j >=1  && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+
+
+
+}
