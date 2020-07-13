6383efe031c4e00cce543c6da8837e7baed3a4b0
liyaupu <liyapu@tusdao.com>
2020-07-13 09:42:40 +0800

    ds

--- data-structure/src/main/java/com/lyp/learn/queue/priorityqueue/Info.java
+++ data-structure/src/main/java/com/lyp/learn/queue/priorityqueue/Info.java
@@ -2,7 +2,7 @@ package com.lyp.learn.queue.priorityqueue;
 
 /**
  *
- *   JAVA中PRIORITYQUEUE详解  小顶堆
+ *   JAVA中 PRIORITYQUEUE 详解  小顶堆
  *   https://www.cnblogs.com/Elliott-Su-Faith-change-our-life/p/7472265.html
  */
 public interface Info {
