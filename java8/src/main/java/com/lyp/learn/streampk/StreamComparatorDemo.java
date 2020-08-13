3243b1982d436013fc67140c36a6154e45a6e5b9
liyaupu <liyapu@tusdao.com>
2020-08-13 17:55:36 +0800

    java8

--- java8/src/main/java/com/lyp/learn/streampk/StreamComparatorDemo.java
+++ java8/src/main/java/com/lyp/learn/streampk/StreamComparatorDemo.java
@@ -1,11 +1,7 @@
 package com.lyp.learn.streampk;
 
 import com.alibaba.fastjson.JSON;
-import com.lyp.learn.bean.User;
-import com.lyp.learn.bean.UserVo;
-import com.lyp.learn.bean.Address;
-import com.lyp.learn.bean.Apple;
-import com.lyp.learn.bean.Student;
+import com.lyp.learn.bean.*;
 import com.lyp.learn.utils.MyBeanUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeanUtils;
@@ -16,25 +12,25 @@ import java.util.stream.Collectors;
 
 public class StreamComparatorDemo {
     List<Apple> inventory = Arrays.asList(
-            new Apple("green",80,"黄土高原"),
-            new Apple("green",200,"黄河故道"),
-            new Apple("red",160,"渤海湾"),
-            new Apple("yellow",20,"渤海湾"),
-            new Apple("green",100,"黄土高原")
+            new Apple("green", 80, "黄土高原"),
+            new Apple("green", 200, "黄河故道"),
+            new Apple("red", 160, "渤海湾"),
+            new Apple("yellow", 20, "渤海湾"),
+            new Apple("green", 100, "黄土高原")
     );
 
     List<Student> studentsSource = Arrays.asList(
-            new Student(10, 20, "aty", new Address("河南省","商丘市",476000)),
-		    new Student(1, 22, "qun", new Address("山东省","临沂",300000)),
-		    new Student(1, 26, "Zen", new Address("河南省","郑州市",400000)),
-		    new Student(5, 23, "Aty", new Address("河南省","开封市",480000))
+            new Student(10, 20, "aty", new Address("河南省", "商丘市", 476000)),
+            new Student(1, 22, "qun", new Address("山东省", "临沂", 300000)),
+            new Student(1, 26, "Zen", new Address("河南省", "郑州市", 400000)),
+            new Student(5, 23, "Aty", new Address("河南省", "开封市", 480000))
     );
 
     /**
      * 排序----按苹果重量升序排序
      */
     @Test
-    public void test1(){
+    public void test1() {
         List<Apple> weightList1 = new ArrayList<>(inventory);
         System.out.println(weightList1);
         Collections.sort(weightList1, new Comparator<Apple>() {
@@ -50,7 +46,7 @@ public class StreamComparatorDemo {
      * List 中排序对象
      */
     @Test
-    public void test2(){
+    public void test2() {
         List<Apple> weightList = new ArrayList<>(inventory);
         System.out.println(weightList);
         weightList.sort(new Comparator<Apple>() {
@@ -64,34 +60,35 @@ public class StreamComparatorDemo {
 
 
     @Test
-    public void test3(){
+    public void test3() {
         List<Apple> weightList = new ArrayList<>(inventory);
         System.out.println(weightList);
-        weightList.sort((Apple a1,Apple a2) -> a1.getWeight().compareTo(a2.getWeight()));
+        weightList.sort((Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight()));
         System.out.println(weightList);
     }
 
     public static List<User> users = new ArrayList<>();
+
     {
-        users.add(new User(1,"张三",20,"13211112222"));
-        users.add(new User(4,"jack",15,"13266668888"));
-        users.add(new User(3,"李四",60,"13299999999"));
-        users.add(new User(2,"jack",10,"1323333444"));
+        users.add(new User(1, "张三", 20, "13211112222"));
+        users.add(new User(4, "jack", 15, "13266668888"));
+        users.add(new User(3, "李四", 60, "13299999999"));
+        users.add(new User(2, "jack", 10, "1323333444"));
     }
 
     @Test
-    public void testSort1(){
+    public void testSort1() {
         List<UserVo> userVoList = users.stream()
                 .map(u -> MyBeanUtils.copyProperties(u, UserVo.class))
                 .sorted(Comparator.comparingInt(UserVo::getAge))
                 .collect(Collectors.toList());
 
         userVoList.stream()
-                  .forEach(System.out::println);
+                .forEach(System.out::println);
     }
 
     @Test
-    public void testSort2(){
+    public void testSort2() {
         List<UserVo> userVoList = users.stream()
                 .map(u -> {
                     UserVo uv = new UserVo();
@@ -106,7 +103,7 @@ public class StreamComparatorDemo {
     }
 
     @Test
-    public void testSort3(){
+    public void testSort3() {
         List<UserVo> userVoList = users.stream()
                 .map(u -> {
                     UserVo uv = MyBeanUtils.copyProperties(u, UserVo.class);
@@ -123,7 +120,7 @@ public class StreamComparatorDemo {
      * comparing
      */
     @Test
-    public void test4(){
+    public void test4() {
         List<Apple> weightList = new ArrayList<>(inventory);
         System.out.println(weightList);
         weightList.sort(Comparator.comparing(apple -> apple.getWeight()));
@@ -132,7 +129,7 @@ public class StreamComparatorDemo {
 
 
     @Test
-    public void test5(){
+    public void test5() {
         List<Apple> weightList = new ArrayList<>(inventory);
         System.out.println(weightList);
         weightList.sort(Comparator.comparing(Apple::getWeight));
@@ -143,7 +140,7 @@ public class StreamComparatorDemo {
      * comparingInt
      */
     @Test
-    public void test51(){
+    public void test51() {
         List<Apple> weightList = new ArrayList<>(inventory);
         System.out.println(weightList);
         weightList.sort(Comparator.comparingInt(Apple::getWeight));
@@ -154,23 +151,23 @@ public class StreamComparatorDemo {
      * 倒序排
      */
     @Test
-    public void test6(){
+    public void test6() {
         List<Apple> weightList = new ArrayList<>(inventory);
         System.out.println(weightList);
-        weightList.sort((Apple a1,Apple a2) -> a2.getWeight().compareTo(a1.getWeight()));
+        weightList.sort((Apple a1, Apple a2) -> a2.getWeight().compareTo(a1.getWeight()));
         System.out.println(weightList);
     }
 
     @Test
-    public void test7(){
+    public void test7() {
         List<Apple> weightList = new ArrayList<>(inventory);
         System.out.println(weightList);
-        weightList.sort(Comparator.comparing(Apple::getWeight,Comparator.reverseOrder()));
+        weightList.sort(Comparator.comparing(Apple::getWeight, Comparator.reverseOrder()));
         System.out.println(weightList);
     }
 
     @Test
-    public void test71(){
+    public void test71() {
         List<Apple> weightList = new ArrayList<>(inventory);
         System.out.println(weightList);
         weightList.sort(Comparator.comparingInt(Apple::getWeight).reversed());
@@ -181,7 +178,7 @@ public class StreamComparatorDemo {
      * 按多个字段进行排序
      */
     @Test
-    public void test10(){
+    public void test10() {
         List<Apple> colorWeightList = new ArrayList<>(inventory);
         colorWeightList.sort(Apple::compareByColorWeight);
         System.out.println(colorWeightList);
@@ -193,15 +190,15 @@ public class StreamComparatorDemo {
      * 先按颜色排序，颜色相同再按重量排序
      */
     @Test
-    public void test100(){
+    public void test100() {
         List<Apple> colorWeightList = new ArrayList<>(inventory);
-        Collections.sort(colorWeightList,(a1,a2)->{
-            if(a1.getColor().equals(a2.getColor())){
+        Collections.sort(colorWeightList, (a1, a2) -> {
+            if (a1.getColor().equals(a2.getColor())) {
                 //正排序
 //                return Integer.compare(a1.getWeight(),a2.getWeight());
                 //倒排序
-                return -Integer.compare(a1.getWeight(),a2.getWeight());
-            }else{
+                return -Integer.compare(a1.getWeight(), a2.getWeight());
+            } else {
                 return a1.getColor().compareTo(a2.getColor());
             }
         });
@@ -214,11 +211,11 @@ public class StreamComparatorDemo {
      * 按多个字段进行排序
      */
     @Test
-    public void test11(){
+    public void test11() {
         List<Apple> colorWeightList = new ArrayList<>(inventory);
         colorWeightList.sort(Comparator.comparing(Apple::getColor)
-                                        .thenComparing(Apple::getWeight)
-                            );
+                .thenComparing(Apple::getWeight)
+        );
         System.out.println(colorWeightList);
         System.out.println(JSON.toJSONString(colorWeightList));
         System.out.println();
@@ -235,12 +232,12 @@ public class StreamComparatorDemo {
      * 要求排序的 Integer,String 必须实现 Comparable 接口，默认已经实现
      */
     @Test
-    public void test20(){
+    public void test20() {
         List<Integer> numList1 = Arrays.asList(1, 4, 2, 6, 2, 8);
         numList1.sort(Comparator.naturalOrder());
         System.out.println(numList1);
 
-        List<String> strList1 = Arrays.asList("d","f","c","a","x","H","D","A","Y");
+        List<String> strList1 = Arrays.asList("d", "f", "c", "a", "x", "H", "D", "A", "Y");
         strList1.sort(Comparator.naturalOrder());
         System.out.println(strList1);
     }
@@ -251,12 +248,12 @@ public class StreamComparatorDemo {
      * 要求排序的 Integer,String 必须实现 Comparable 接口，默认已经实现
      */
     @Test
-    public void test21(){
+    public void test21() {
         List<Integer> numList1 = Arrays.asList(1, 4, 2, 6, 2, 8);
         numList1.sort(Comparator.reverseOrder());
         System.out.println(numList1);
 
-        List<String> strList1 = Arrays.asList("d","f","c","a","x","H","D","A","Y");
+        List<String> strList1 = Arrays.asList("d", "f", "c", "a", "x", "H", "D", "A", "Y");
         strList1.sort(Comparator.reverseOrder());
         System.out.println(strList1);
     }
@@ -267,7 +264,7 @@ public class StreamComparatorDemo {
      *  流中比较对象
      */
     @Test
-    public void test80(){
+    public void test80() {
         // 使用lambda表达式创建Function对象
         Function<Student, Integer> extractIdWay1 = (student) -> student.getId();
 
@@ -279,20 +276,20 @@ public class StreamComparatorDemo {
 
         // 升序
         List<Student> ascList = studentsSource.stream()
-                                                .sorted(byId)
-                                                .collect(Collectors.toList());
+                .sorted(byId)
+                .collect(Collectors.toList());
         System.out.println(ascList);
 
         List<Student> ascList2 = studentsSource.stream()
-                                                .sorted(Comparator.comparingInt(Student::getId))
-                                                .collect(Collectors.toList());
+                .sorted(Comparator.comparingInt(Student::getId))
+                .collect(Collectors.toList());
         System.out.println(ascList2);
 
 
         // 降序
         List<Student> descList = studentsSource.stream()
-                                                .sorted(byId.reversed())
-                                                .collect(Collectors.toList());
+                .sorted(byId.reversed())
+                .collect(Collectors.toList());
         System.out.println(descList);
     }
 
@@ -300,26 +297,26 @@ public class StreamComparatorDemo {
      * 由于Student.getAddress()返回的对象没有实现Comparable接口，所以不能通过Comparator.comparing()创建一个Comparator对象
      * //下面的代码编译不通过
      *  List<Student> ascList = studentsSource.stream()
-    *                                           .sorted(Comparator.comparing(Student::getAddress))
-    *                                           .collect(Collectors.toList());
+     *                                           .sorted(Comparator.comparing(Student::getAddress))
+     *                                           .collect(Collectors.toList());
      * 如果我们想按照Address的province字段(没有实现Comparable接口)排序怎么办呢？使用另一种形式的comparing方法：
      *
      *  *@param keyExtractor the function used to extract the sort key
      *  *@param  keyComparator the {@code Comparator} used to compare the sort key , keyComparator 是创建一个自定义的比较器
      */
     @Test
-    public void test81(){
+    public void test81() {
         Comparator<Address> cmpPro = Comparator.comparing(Address::getProvince);
         Comparator<Student> byAddress = Comparator.comparing(Student::getAddress, cmpPro);
         List<Student> sortedAddressList = studentsSource.stream()
-                                                        .sorted(byAddress)
-                                                        .collect(Collectors.toList());
+                .sorted(byAddress)
+                .collect(Collectors.toList());
         //这种形式的comparing()接收2个参数，第一个参数提取要排序的key，第二个参数指定排序的Comparator。
         System.out.println(sortedAddressList);
         System.out.println();
 
         List<Student> sortAddressList2 = studentsSource.stream()
-                .sorted(Comparator.comparing(Student::getAddress,Comparator.comparing(Address::getProvince)))
+                .sorted(Comparator.comparing(Student::getAddress, Comparator.comparing(Address::getProvince)))
                 .collect(Collectors.toList());
         System.out.println(sortAddressList2);
         System.out.println();
@@ -329,8 +326,8 @@ public class StreamComparatorDemo {
 
         Comparator<Student> byName = Comparator.comparing(Student::getName, String.CASE_INSENSITIVE_ORDER);
         List<Student> sortedNameList = studentsSource.stream()
-                                                    .sorted(byName)
-                                                    .collect(Collectors.toList());
+                .sorted(byName)
+                .collect(Collectors.toList());
         System.out.println(studentsSource);
         System.out.println(sortedNameList);
     }
@@ -339,18 +336,18 @@ public class StreamComparatorDemo {
      * 按多个字段进行排序
      */
     @Test
-    public void test83(){
+    public void test83() {
         // id升序
         Comparator<Student> byIdASC = Comparator.comparing(Student::getId);
         // named不分区大小写降序
         Comparator<Student> byNameDESC = Comparator.comparing(Student::getName, String.CASE_INSENSITIVE_ORDER)
-                                                    .reversed();
+                .reversed();
         // 联合排序
         Comparator<Student> finalComparator = byIdASC.thenComparing(byNameDESC);
 
         List<Student> result = studentsSource.stream()
-                                                .sorted(finalComparator)
-                                                .collect(Collectors.toList());
+                .sorted(finalComparator)
+                .collect(Collectors.toList());
         System.out.println(result);
 
     }
@@ -363,17 +360,173 @@ public class StreamComparatorDemo {
      * null元素大于非空元素，null元素排后面
      */
     @Test
-    public void test22(){
-        List<String> strList1 = Arrays.asList("d","a",null,"x","D",null,"A");
+    public void test22() {
+        List<String> strList1 = Arrays.asList("d", "a", null, "x", "D", null, "A");
         strList1.sort(Comparator.nullsFirst(Comparator.naturalOrder()));
         System.out.println(strList1);
 
-        List<String> strList2 = Arrays.asList("d","a",null,"x","D",null,"A");
+        List<String> strList2 = Arrays.asList("d", "a", null, "x", "D", null, "A");
         strList2.sort(Comparator.nullsLast(Comparator.naturalOrder()));
         System.out.println(strList2);
-
     }
 
+
+    /**
+     * list 使用 map 去重，然后排序
+     */
+    @Test
+    public void test23() {
+        List<Worker> Workers = getWorkerList();
+        //利用Collectors.toMap去重
+        //values.stream id排序的功能
+        String nameMsg = Workers.stream()
+                .filter(s -> s.getAge() <= 100)
+                //此步骤可以根据 id  去重
+                .collect(Collectors.toMap(Worker::getId, Function.identity(), (oldValue, newValue) -> oldValue))
+                .values()
+                .stream()
+                //根据 id 排序
+                .sorted(Comparator.comparing(Worker::getId))
+                .map(Worker::getName)
+                .collect(Collectors.joining("|"));
+        System.out.println(nameMsg);
+    }
+
+    /**
+     *  list 转 map，value 为 list
+     *  然后 value list  转为 string
+     */
+    @Test
+    public void test24(){
+        List<Worker> workers = getWorkerList();
+        //获取 地址 对应的 工人列表
+        Map<String, List<Worker>> address$WorkMap = workers.stream()
+                .collect(Collectors.groupingBy(Worker::getAddress));
+
+        //获取 地址 对应的工人列表对应的字符串名称
+        Map<String, String> address$namesStr = address$WorkMap.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        e -> e.getKey(),
+                        e -> e.getValue()
+                                .stream()
+                                .map(c -> c.getName())
+                                .collect(Collectors.joining(","))
+                        )
+                );
+
+        // 拼接为地址:工人名称列表
+        StringBuilder msgContentSb = new StringBuilder();
+        address$namesStr.entrySet()
+                .stream()
+                //根据 key 排序
+                .sorted(Map.Entry.comparingByKey())
+                //有序遍历
+                .forEachOrdered((entry) ->{
+                    msgContentSb.append("|")
+                            .append(entry.getKey())
+                            .append(":")
+                            .append(entry.getValue());
+                });
+        String msgContent = msgContentSb.toString();
+        System.out.println(msgContent);
+    }
+
+    private List<Worker> getWorkerList() {
+        List<Worker> Workers = new ArrayList<>();
+        Workers.add(new Worker(5, "关羽", 22, "北京"));
+        Workers.add(new Worker(8, "张飞", 11, "天津"));
+        Workers.add(new Worker(8, "张飞", 11, "天津"));
+        Workers.add(new Worker(2, "曹操", 56, "上海"));
+        Workers.add(new Worker(9, "刘备", 32, "北京"));
+        Workers.add(new Worker(1, "诸葛亮", 26, "温州"));
+        Workers.add(new Worker(9, "刘备", 32, "北京"));
+        Workers.add(new Worker(9, "刘备", 32, "北京"));
+        Workers.add(new Worker(4, "孙权", 18, "上海"));
+        return Workers;
+    }
+
+    public void printList(List<Worker> Workers) {
+        for (Worker Worker : Workers) {
+            System.out.println(Worker);
+        }
+        System.out.println();
+    }
+
+    class Worker {
+        private Integer id;
+        private String name;
+        private Integer age;
+        private String address;
+
+        public Worker() {
+        }
+
+        public Worker(Integer id, String name, Integer age, String address) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Worker Worker = (Worker) o;
+            return id.equals(Worker.id) &&
+                    name.equals(Worker.name) &&
+                    age.equals(Worker.age) &&
+                    address.equals(Worker.address);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, age, address);
+        }
+
+        @Override
+        public String toString() {
+            return "Worker{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    ", address='" + address + '\'' +
+                    '}';
+        }
+    }
 
 
 }
---
+++ java8/src/main/java/com/lyp/learn/streampk/StreamDistinctDemo.java
@@ -0,0 +1,264 @@
+package com.lyp.learn.streampk;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Stream 的distinct()方法
+ *     distinct()是Java 8 中 Stream 提供的方法，返回的是由该流中不同元素组成的流。
+ *
+ *     distinct()使用 hashCode() 和 eqauls() 方法来获取不同的元素。
+ *     因此，需要去重的类必须实现 hashCode() 和 equals() 方法。
+ *     换句话讲，我们可以通过重写定制的 hashCode() 和 equals() 方法来达到某些特殊需求的去重
+ *
+ *     distinct（）返回由该流的不同元素组成的流。
+ *     distinct（）是Stream接口的方法。distinct（）使用hashCode（）和equals（）方法来获取不同的元素。
+ *     因此，我们的类必须实现hashCode（）和equals（）方法。
+ *
+ *     如果distinct（）正在处理有序流，那么对于重复元素，将保留以遭遇顺序首先出现的元素，并且以这种方式选择不同元素是稳定的。
+ *
+ *     在无序流的情况下，不同元素的选择不一定是稳定的，是可以改变的。
+ *
+ *     distinct（）执行有状态的中间操作。在有序流的并行流的情况下，保持distinct（）的稳定性是需要很高的代价的，
+ *     因为它需要大量的缓冲开销。
+ *
+ *     如果我们不需要保持遭遇顺序的一致性，那么我们应该可以使用通过BaseStream.unordered（）方法实现的无序流。
+ */
+public class StreamDistinctDemo {
+
+    /**
+     * 对于 String 列表的去重
+     * 因为 String 类已经覆写了 equals() 和 hashCode() 方法，所以可以去重成功
+     */
+    @Test
+    public void test01() {
+        List<String> stringList = new ArrayList<String>() {{
+            add("A");
+            add("A");
+            add("B");
+            add("B");
+            add("A");
+            add("A");
+            add("C");
+        }};
+
+        System.out.println("去重前：");
+        for (String s : stringList) {
+            System.out.println(s);
+        }
+        System.out.println();
+
+        stringList = stringList.stream().distinct().collect(Collectors.toList());
+        System.out.println("去重后：");
+        for (String s : stringList) {
+            System.out.println(s);
+        }
+        System.out.println();
+    }
+
+    /**
+     * 对于实体类列表的去重
+     *  需要重写 equals() 以及 hashCode() 方法
+     */
+    @Test
+    public void test02() {
+        List<Student> studentList = getStudentList();
+        System.out.println("去重前：");
+        System.out.println(studentList);
+
+        List<Student> newList = studentList
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println("去重后：");
+        System.out.println(newList);
+    }
+
+    /**
+     *   根据 List<Object> 中 Object 某个属性去重
+     *     根据名字去重后
+     */
+    @Test
+    public void test03() {
+        List<Student> studentList = getStudentList();
+        System.out.println("去重前：");
+        System.out.println(studentList);
+
+        //先根据 equals 和 hashCode 去重
+        List<Student> newList = studentList
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println("去重后：");
+        System.out.println(newList);
+
+        // 然后再根据 地址 address 去重
+
+
+        //通过 TreeSet<> 来达到获取不同元素的效果
+        List<Student> nl = newList.stream().collect(
+                Collectors.collectingAndThen(
+                        Collectors.toCollection(
+                                  () -> new TreeSet<>(
+                                          Comparator.comparing(Student::getAddress)
+                                  )),
+                        ArrayList::new)
+        );
+        System.out.println("根据地址去重后 :");
+        System.out.println(nl);
+    }
+
+    @Test
+    public void test031() {
+        List<Student> studentList = getStudentList();
+        System.out.println("去重前：");
+        printList(studentList);
+
+        // 然后再根据 地址 address 去重
+
+
+        //通过 TreeSet<> 来达到获取不同元素的效果
+        //根据 年龄和姓名 两个字段去重
+        List<Student> newList = studentList.stream().collect(
+                Collectors.collectingAndThen(
+                        Collectors.toCollection(
+                                () -> new TreeSet<>(
+                                        Comparator.comparing(s -> s.getAge() + "-" + s.getName())
+                                )),
+                        ArrayList::new)
+        );
+
+        System.out.println("根据 年龄和姓名 去重后 :");
+        printList(newList);
+    }
+
+
+    /**
+     *   根据 List<Object> 中 Object 某个属性去重
+     *     通过 filter() 方法
+     *
+     *     我们首先创建一个方法作为 Stream.filter() 的参数，其返回类型为 Predicate，
+     *     原理就是判断一个元素能否加入到 Set 中去
+     */
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
+    }
+
+    @Test
+    public void test04() {
+        List<Student> studentList = getStudentList();
+        System.out.println("去重前：");
+        System.out.println(studentList);
+
+        //这里我们将 distinctByKey() 方法作为 filter() 的参数，过滤掉那些不能加入到 set 的元素
+        List<Student> newList = studentList.stream()
+                .filter(distinctByKey(Student::getAddress))
+                .collect(Collectors.toList());
+        System.out.println("根据地址去重后：");
+        System.out.println(newList);
+    }
+
+    private List<Student> getStudentList() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(5, "关羽", 22, "北京"));
+        students.add(new Student(8, "张飞", 11, "天津"));
+        students.add(new Student(8, "张飞", 11, "天津"));
+        students.add(new Student(2, "曹操", 56, "上海"));
+        students.add(new Student(9, "刘备", 32, "北京"));
+        students.add(new Student(1, "诸葛亮", 26, "温州"));
+        students.add(new Student(9, "刘备", 32, "北京"));
+        students.add(new Student(9, "刘备", 32, "北京"));
+        students.add(new Student(4, "孙权", 18, "上海"));
+        return students;
+    }
+
+    public void printList(List<Student> students){
+        for (Student student : students) {
+            System.out.println(student);
+        }
+        System.out.println();
+    }
+
+    class Student {
+        private Integer id;
+        private String name;
+        private Integer age;
+        private String address;
+
+        public Student() {
+        }
+
+        public Student(Integer id, String name, Integer age, String address) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Student student = (Student) o;
+            return id.equals(student.id) &&
+                    name.equals(student.name) &&
+                    age.equals(student.age) &&
+                    address.equals(student.address);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, age, address);
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    ", address='" + address + '\'' +
+                    '}';
+        }
+    }
+}
+
+
