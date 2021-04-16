feff8cf9f8977dad4f8ed135026b617141f74306
liyaupu <liyapu@tusdao.com>
2021-04-16 17:57:49 +0800

    lee

---
+++ data-structure/src/main/java/com/lyp/likou/link/S_25.java
@@ -0,0 +1,37 @@
+package com.lyp.likou.link;
+
+/**
+ *@author: liyapu
+ *@description:
+ *@date 2021-04-16 17:55
+ *
+ * 剑指 Offer 25. 合并两个排序的链表
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ *
+ * 示例1：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 限制：
+ *
+ * 0 <= 链表长度 <= 1000
+ *
+ * 注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ */
+public class S_25 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+}
