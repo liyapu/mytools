1815ad1edd2c05f78e772ff67633eb829997a9af
liyaupu <liyapu@tusdao.com>
2021-05-03 01:06:48 +0800

    lett

---
+++ data-structure/src/main/java/com/lyp/likou/array/A_704.java
@@ -0,0 +1,57 @@
+package com.lyp.likou.array;
+
+/**
+ *@author: liyapu
+ *@description:
+ *@date 2021-05-03 00:52
+ *
+ * 704. 二分查找
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+ * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ *
+ *
+ * 示例 1:
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ *
+ * 示例 2:
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ *
+ *
+ * 提示：
+ *
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ */
+public class A_704 {
+    public static int search(int[] nums, int target) {
+        int len;
+        if (nums == null || (len = nums.length) == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = len - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 9));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 6));
+    }
+}
