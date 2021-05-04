b592d71bb4877487c981bd384a558d6999b786a3
liyapugitglobal <liyapu20116197@163.com>
2021-05-04 16:15:49 +0800

    print

--- codeLearn/src/main/java/com/lyp/learn/base/threads/pk05/PrintOddEven2.java
+++ codeLearn/src/main/java/com/lyp/learn/base/threads/pk05/PrintOddEven2.java
@@ -4,6 +4,16 @@ package com.lyp.learn.base.threads.pk05;
  * 两个线程交替打印奇偶数
  * odd  奇数
  * even 偶数
+ *
+ * 当一个线程打印进入时，使用wait()进行阻塞该线程；
+ * 另外一个线程进入时，在进入利用notify() 或者是notifyAll()进行释放线程。
+ *
+ * wait()：一旦执行此方法，当前线程就会进入阻塞状态，并释放同步监视器。
+ * notify():一旦执行此方法，就会唤醒被wait()的一个线程，如果有多个线程就会唤醒优先级高的那个。
+ * notifyAll():一旦执行此方法，就会唤醒所有被wait()的所有的线程。
+ * 说明：
+ *    三个方法必须使用在同步代码块中或者是使用在同步方法中
+ *    三个方法的调用者必须是同步代码块或者是同步方法中的同步监视器，否则会出现 IllegalMonitorStateException 异常
  */
 public class PrintOddEven2 {
 
