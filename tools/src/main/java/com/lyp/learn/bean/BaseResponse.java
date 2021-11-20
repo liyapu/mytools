1459baf3b07182034f2bb66e87d312456e0323ec
liyapugitglobal <liyapu20116197@163.com>
2021-11-21 01:48:16 +0800

    mas

--- tools/src/main/java/com/lyp/learn/bean/BaseResponse.java
+++ tools/src/main/java/com/lyp/learn/bean/BaseResponse.java
@@ -1,11 +1,5 @@
 package com.lyp.learn.bean;
 
-import com.facebook.swift.codec.ThriftField;
-import com.facebook.swift.codec.ThriftStruct;
-import com.meituan.servicecatalog.api.annotations.FieldDoc;
-import com.meituan.servicecatalog.api.annotations.Requiredness;
-import com.meituan.servicecatalog.api.annotations.TypeDoc;
-import lombok.ToString;
 
 /**
  * @Description 基本返回对象
