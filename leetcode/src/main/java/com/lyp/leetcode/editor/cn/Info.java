a2385a0456bfea97b1ed1eda880402d8375b3eb0
liyapu <liyapu@meituan.com>
2023-06-11 18:44:36 +0800

    leetcode

---
+++ leetcode/src/main/java/com/lyp/leetcode/editor/cn/Info.java
@@ -0,0 +1,40 @@
+package com.lyp.leetcode.editor.cn;
+
+/**
+ * @author liyapu
+ * @date 2023-06-11 18:36
+ * @description
+ */
+public interface Info {
+
+//        自己计划放的包路径
+//        package com.lyp.leetcode.editor.cn;
+//
+//        设置
+//        TempFilePath:  /Users/liyapu/mywork/repositoryMy/mytools/leetcode/src/main/java/com/lyp
+//        codeFileNaem:  P$!{question.frontendQuestionId}_$!velocityTool.camelCaseName(${question.titleSlug})
+//        CodeTemplate
+//        package com.lyp.leetcode.editor.cn;
+//
+//        ${question.content}
+//
+//        /**
+//         * ${question.title}
+//         * @date $!velocityTool.date()
+//         */
+//        public class P${question.frontendQuestionId}_$!velocityTool.camelCaseName(${question.titleSlug}){
+//          public static void main(String[] args) {
+//              //测试代码
+//              Solution solution = new P$!{question.frontendQuestionId}_$!velocityTool.camelCaseName(${question.titleSlug})().new Solution();
+//          }
+//
+//        //力扣代码
+//        ${question.code}
+//        }
+//
+//
+//
+//
+
+
+}
