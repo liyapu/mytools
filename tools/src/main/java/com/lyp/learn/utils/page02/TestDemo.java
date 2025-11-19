bff2550a36a3faabc4532cd866ea0b56e1dbd09d
liyapu <liyapu@meituan.com>
2025-11-19 11:59:31 +0800

    feat(util): 添加测试

--- tools/src/main/java/com/lyp/learn/utils/page02/TestDemo.java
+++ tools/src/main/java/com/lyp/learn/utils/page02/TestDemo.java
@@ -1,5 +1,7 @@
 package com.lyp.learn.utils.page02;
 
+import java.util.List;
+
 /**
  * @author liyapu
  * @date 2025-11-18 15:49
@@ -13,6 +15,26 @@ public class TestDemo {
 //        return PageQueryUtil.queryAllResultByPage(pageQuery, pageSize, String.format("查询所有修改的sku记录死循环,fdcId:%s",
 //                fdcId));
 //    }
+
+//    /**
+//     * 分页查询已存在的配置
+//     */
+//    private List<NewProductSafeStockCityConfigPO> selectExistingConfigsByPage(NewProductSafeStockCityConfigQueryParam queryParam) {
+//        // 使用 PageQuery 接口定义查询逻辑
+//        PageQuery<NewProductSafeStockCityConfigPO> pageQuery = (offset, limit) -> {
+//            queryParam.setOffset(offset);
+//            queryParam.setLimit(limit);
+//            return newProductSafeStockCityConfigRepository.selectAllDataByParam(queryParam);
+//        };
+//
+//        // 使用 PageQueryUtil 工具类进行分页查询
+//        return PageQueryUtil.queryAllResultByPage(
+//                pageQuery,
+//                CommonTool.GROUP_NUM_200,
+//                "查询新品安全库存配置死循环"
+//        );
+//    }
+
 
 
 //    public List<FdcDemandPlanSimpleDTO> queryByPagePoiId(Long poiId, String createDateStr) {
